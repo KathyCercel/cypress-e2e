4adba94ad6dfa54414b6ad3ca7d14dae377cbe11
Teo <teo@Teos-MacBook-Pro.local>
2025-02-20 10:57:42 +0000

    adding more tests for login and improve cypress config

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,4 +14,35 @@ describe('SauceDemo Login Tests', () => {
       });
     });
   });
+
+  it('should display an error message for invalid credentials', () => {
+
+    loginPage.enterUsername('invalid_user');
+    loginPage.enterPassword('wrong_password');
+    loginPage.clickLoginButton();
+
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain', "Epic sadface: Username and password do not match any user in this service");
+  });
+
+  it('should prevent login when username is empty', () => {
+    loginPage.enterUsername('abc');
+
+    loginPage.clickLoginButton();
+
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain', 'Epic sadface: Password is required');
+
+  });
+
+  it('should prevent login when password is empty', () => {
+    loginPage.enterPassword('abc');
+    loginPage.clickLoginButton();
+
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain', 'Epic sadface: Username is required');
+  });
 });
