d8a9730b38ca6b49db130087a30e0d246fabc8c2
KathyCercel <kathy.cercel@gmail.com>
2025-02-20 12:30:13 +0000

    tests against auth via localStorage and cookies

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 import { loginPage, menuButton, inventoryPage } from '../support/pages/index';
+import { getPassword } from '../support/utils';
 
 describe('SauceDemo Login Tests', () => {
   beforeEach(() => {
@@ -81,7 +82,7 @@ describe('Session Handling Tests', () => {
     cy.reload();
 
     cy.url().should('include', '/cart.html');
-    
+
     cy.get('.title').should('contain', 'Your Cart');
 
     cy.getCookies().should('not.be.empty');
@@ -103,5 +104,95 @@ describe('Session Handling Tests', () => {
 
     cy.reload();
     cy.url().should('eq', 'https://www.saucedemo.com/');
+  });
+});
+
+/**
+ * SauceDemo handles authentication entirely on the frontend using:
+ * - React state (`useState`)
+ * - `setCredentials(username, password)`
+ * - `verifyCredentials()`
+ * 
+ * There is no actual API request (`POST /login`).
+ * Authentication is managed via **localStorage or cookies** instead.
+ * 
+ * How to Validate Login & Session in Cypress:
+ * Simulate login via the UI by entering valid credentials.
+ * Verify session persistence by checking `localStorage` and cookies.
+ * Ensure the user remains logged in after navigating between pages and reloading.
+ */
+
+describe("SauceDemo Login & Session Handling", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should log in and persist the session", () => {
+
+    loginPage.enterUsername("standard_user");
+    getPassword().then((password) => {
+      loginPage.enterPassword(password);
+      loginPage.clickLoginButton();
+    });
+
+    cy.url().should("include", "/inventory.html");
+    cy.get(".title").should("contain", "Products");
+
+    cy.window().then((win) => {
+      cy.log("LocalStorage Data:", win.localStorage);
+      expect(win.localStorage.length).to.eq(2);
+    });
+
+    cy.getCookies().then((cookies) => {
+      cy.log("Cookies:", cookies);
+      expect(cookies.length).to.eq(1);
+    });
+
+    cy.reload();
+    cy.url().should("include", "/inventory.html");
+    cy.get(".title").should("contain", "Products");
+
+    inventoryPage.getShoppingCartLink().click();
+    cy.url().should('include', '/cart.html');
+
+    cy.window().then((win) => {
+      cy.log("LocalStorage Data:", win.localStorage);
+      expect(win.localStorage.length).to.eq(2);
+    });
+  });
+
+  it("should fail login for an invalid user and show an error", () => {
+    loginPage.enterUsername("invalid_user");
+    loginPage.enterPassword("wrong_password");
+    loginPage.clickLoginButton();
+
+    loginPage.getErrorMessage()
+      .should("be.visible")
+      .and("contain", "Epic sadface: Username and password do not match any user in this service");
+  });
+
+  it("should log out the user and clear the session", () => {
+
+    loginPage.enterUsername("standard_user");
+    getPassword().then((password) => {
+      loginPage.enterPassword(password);
+      loginPage.clickLoginButton();
+    });
+
+    menuButton.getOpenMenu().click();
+    menuButton.getLogoutSidebarLink().click();
+
+    cy.url().should("eq", "https://www.saucedemo.com/");
+
+    cy.getCookies().should("be.empty");
+
+    cy.window().then((win) => {
+      win.localStorage.clear();
+      expect(win.localStorage.length).to.eq(0);
+    });
+
+    cy.reload();
+    cy.url().should("eq", "https://www.saucedemo.com/");
+    loginPage.getUserNameSelector().should("be.visible");
   });
 });
